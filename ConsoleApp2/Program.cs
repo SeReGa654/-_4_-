f5cf27ef65b31235cfaadfbfac4dc0abbd44c3ff
SERHII SOLONINKO <102055965+SeReGa654@users.noreply.github.com>
2024-03-15 12:23:44 +0200

    2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть дату у форматi YYYY-MM-DD:");
+        string inputDate = Console.ReadLine();
+
+        DateTime date;
+        if (DateTime.TryParse(inputDate, out date))
+        {
+            DayOfWeek dayOfWeek = date.DayOfWeek;
+
+            Console.WriteLine($"День тижня: {dayOfWeek}");
+        }
+        else
+        {
+            Console.WriteLine("Неправильний формат дати. Введiть дату у форматi YYYY-MM-DD.");
+        }
+    }
+}
